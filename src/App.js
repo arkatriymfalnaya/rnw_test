6f10a58ef4c84f07142074f64006a569568e2273
dog <dog@arkatriymfalnaya.local>
2022-05-24 19:45:06 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Text } from "react-native"
+
+// const TELEGRAM_TOKEN = ''
+// const BOT_URI = `https://api.telegram.org/bot<${TELEGRAM_TOKEN}>/test/METHOD_NAME`
+// const WebApp = window.Telegram.WebApp
+
+function App() {
+  return (
+      <Text>init</Text>
+    )
+}
+
+export default App
